877ecf6dd6a2e63984318bc6dd21780174f74e6d
joseluis <joseljg@gmail.com>
2020-10-21 17:44:56 +0200

    calculadora v1

---
+++ app/src/main/java/es/joseljg/calculadoraamarilla/MainActivity.java
@@ -0,0 +1,169 @@
+package es.joseljg.calculadoraamarilla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edt_numero1 = null;
+    EditText edt_numero2 = null;
+    TextView txt_resultado = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edt_numero1 = findViewById(R.id.edt_numero1);
+        edt_numero2 = findViewById(R.id.edt_numero2);
+        txt_resultado = findViewById(R.id.txt_resultado);
+    }
+
+    public void sumar(View view) {
+        double numero1 = 0;
+        try {
+            String textoNumero1 = edt_numero1.getText().toString();
+            numero1 = Double.valueOf(textoNumero1);
+        }
+        catch (Exception e) {
+             edt_numero1.setError("numero1 incorrecto");
+             return;
+        }
+        //---------------------------------------------------------
+        double numero2 = 0;
+        try {
+            String textoNumero2 = edt_numero2.getText().toString();
+            numero2 = Double.valueOf(textoNumero2);
+        }
+        catch (Exception e) {
+            edt_numero2.setError("numero2 incorrecto");
+            return;
+        }
+        //---------------------------------------------------------
+        double resultado = numero1 + numero2;
+        //---------------------------------------------------------
+        double resultadoRedondeado = Math.round(resultado * 100.0)/ 100.0;
+        //---------------------------------------------------------------
+        txt_resultado.setText(String.valueOf(resultadoRedondeado));
+    }
+
+    public void restar(View view) {
+        double numero1 = 0;
+        try {
+            String textoNumero1 = edt_numero1.getText().toString();
+            numero1 = Double.valueOf(textoNumero1);
+        }
+        catch (Exception e) {
+            edt_numero1.setError("numero1 incorrecto");
+            return;
+        }
+        //---------------------------------------------------------
+        double numero2 = 0;
+        try {
+            String textoNumero2 = edt_numero2.getText().toString();
+            numero2 = Double.valueOf(textoNumero2);
+        }
+        catch (Exception e) {
+            edt_numero2.setError("numero2 incorrecto");
+            return;
+        }
+        //---------------------------------------------------------
+        double resultado = numero1 - numero2;
+        //---------------------------------------------------------
+        double resultadoRedondeado = Math.round(resultado * 100.0)/ 100.0;
+        //---------------------------------------------------------------
+        txt_resultado.setText(String.valueOf(resultadoRedondeado));
+    }
+
+    public void multiplicar(View view) {
+        double numero1 = 0;
+        try {
+            String textoNumero1 = edt_numero1.getText().toString();
+            numero1 = Double.valueOf(textoNumero1);
+        }
+        catch (Exception e) {
+            edt_numero1.setError("numero1 incorrecto");
+            return;
+        }
+        //---------------------------------------------------------
+        double numero2 = 0;
+        try {
+            String textoNumero2 = edt_numero2.getText().toString();
+            numero2 = Double.valueOf(textoNumero2);
+        }
+        catch (Exception e) {
+            edt_numero2.setError("numero2 incorrecto");
+            return;
+        }
+        //---------------------------------------------------------
+        double resultado = numero1 * numero2;
+        //---------------------------------------------------------
+        double resultadoRedondeado = Math.round(resultado * 100.0)/ 100.0;
+        //---------------------------------------------------------------
+        txt_resultado.setText(String.valueOf(resultadoRedondeado));
+    }
+
+    public void dividir(View view) {
+        double numero1 = 0;
+        try {
+            String textoNumero1 = edt_numero1.getText().toString();
+            numero1 = Double.valueOf(textoNumero1);
+        }
+        catch (Exception e) {
+            edt_numero1.setError("numero1 incorrecto");
+            return;
+        }
+        //---------------------------------------------------------
+        double numero2 = 0;
+        try {
+            String textoNumero2 = edt_numero2.getText().toString();
+            numero2 = Double.valueOf(textoNumero2);
+        }
+        catch (Exception e) {
+            edt_numero2.setError("numero2 incorrecto");
+            return;
+        }
+        if(numero2 == 0)
+        {
+            edt_numero2.setError("el numero2 no puede ser cero");
+            return;
+        }
+        //---------------------------------------------------------
+        double resultado = numero1 / numero2;
+        //---------------------------------------------------------
+        double resultadoRedondeado = Math.round(resultado * 100.0)/ 100.0;
+        //---------------------------------------------------------------
+        txt_resultado.setText(String.valueOf(resultadoRedondeado));
+    }
+
+    public void resto(View view) {
+        double numero1 = 0;
+        try {
+            String textoNumero1 = edt_numero1.getText().toString();
+            numero1 = Double.valueOf(textoNumero1);
+        }
+        catch (Exception e) {
+            edt_numero1.setError("numero1 incorrecto");
+            return;
+        }
+        //---------------------------------------------------------
+        double numero2 = 0;
+        try {
+            String textoNumero2 = edt_numero2.getText().toString();
+            numero2 = Double.valueOf(textoNumero2);
+        }
+        catch (Exception e) {
+            edt_numero2.setError("numero2 incorrecto");
+            return;
+        }
+        //---------------------------------------------------------
+        double resultado = numero1 % numero2;
+        //---------------------------------------------------------
+        double resultadoRedondeado = Math.round(resultado * 100.0)/ 100.0;
+        //---------------------------------------------------------------
+        txt_resultado.setText(String.valueOf(resultadoRedondeado));
+    }
+}
